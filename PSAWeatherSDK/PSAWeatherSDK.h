31bd684648a627042d0a3ef8b6f6cb6638f96d96
mohamed <mohamedamine.belfkih@esprit.tn>
2021-01-16 18:35:15 +0100

    Initial Commit

---
+++ PSAWeatherSDK/PSAWeatherSDK.h
@@ -0,0 +1,18 @@
+//
+//  PSAWeatherSDK.h
+//  PSAWeatherSDK
+//
+//  Created by Mohamed Lamine Belfekih on 16/01/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PSAWeatherSDK.
+FOUNDATION_EXPORT double PSAWeatherSDKVersionNumber;
+
+//! Project version string for PSAWeatherSDK.
+FOUNDATION_EXPORT const unsigned char PSAWeatherSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PSAWeatherSDK/PublicHeader.h>
+
+
