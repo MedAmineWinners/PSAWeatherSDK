c56ec3c75e6f0d3de6144d3a07599ebe7d13daf7
mohamed <mohamedamine.belfkih@esprit.tn>
2021-01-17 15:18:50 +0100

    [PSAWeatherSDK-addCity] add missing commit NetworkSessionMock for unitTest

---
+++ PSAWeatherSDKTests/RemoteServiceTests/NetworkSessionMock.swift
@@ -0,0 +1,19 @@
+//
+//  NetworkSessionMock.swift
+//  PSAWeatherSDKTests
+//
+//  Created by Mohamed Lamine Belfekih on 17/01/2021.
+//
+
+import XCTest
+@testable import PSAWeatherSDK
+
+class NetworkSessionMock: NetworkSession {
+    var data: Data?
+    var error: Error?
+
+    func loadData(from url: URL,
+                  completionHandler: @escaping (Data?, Error?) -> Void) {
+        completionHandler(data, error)
+    }
+}
